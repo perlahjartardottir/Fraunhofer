f7e4eb14703686820a9447f73b6c023d4bcf1559
Freyr Fridfinnsson <freyr12@ru.is>
2015-07-31 08:59:11 -0400

    a little something on add order item view

--- Purchasing/Views/addOrderItem.php
+++ Purchasing/Views/addOrderItem.php
@@ -1,10 +1,30 @@
-<?php session_start(); ?>
+<?php
+include '../../connection.php';
+session_start();
+$sql = "SELECT order_ID
+        FROM purchase_order;";
+$result = mysqli_query($link, $sql);
+?>
 <head>
   <title>Fraunhofer CCD</title>
 </head>
 <body>
   <?php include '../header.php'; ?>
   <div class='container'>
-    <h4>Order items</h4>
+    <div class='row well well-lg'>
+      <form>
+        <div class='form-group col-md-3'>
+          <label>Purchase order: </label>
+          <select class='form-control'>
+            <?
+            while($row = mysqli_fetch_array($result)){
+              var_dump($row[0]);
+              echo"<option value='".$row[0]."'>".$row[0]."</option>";
+            }
+            ?>
+          </select>
+        </div>
+      </form>
+    </div>
   </div>
 </body>
