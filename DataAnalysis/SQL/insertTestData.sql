090a165303d3a19b5052345d0be62472f62a4d76
Perla Hjartardottir <phjartardottir@fraunhofer.org>
2016-06-20 16:46:11 -0400

    diagram and test data

---
+++ DataAnalysis/SQL/insertTestData.sql
@@ -0,0 +1,34 @@
+USE DA_test;
+
+/*SELECT *
+FROM process_equipment;
+
+INSERT INTO process_equipment(prcs_eq_name, prcs_eq_acronym, prcs_eq_active)
+	VALUES("PVD Coating Chamber K1", "K1", TRUE),
+	("PVD Coating Chamber K2", "K2", TRUE),
+    ("PVD Coating Chamber LA", "LA", TRUE),
+    ("PVD Coating Chamber PVD75", "PVD75", TRUE);
+    */
+
+/*INSERT INTO anlys_equipment(anlys_eq_name, anlys_eq_active)
+	VALUES("Dektak Profilemeter", TRUE),
+    ("Calotte Grinder", TRUE),
+    ("Microscope (stereo)", TRUE),
+    ("Hardness Tester (adhesion)", TRUE);
+    */
+
+/*SELECT *
+FROM anlys_equipment;
+
+
+INSERT INTO anlys_property(anlys_eq_ID,anlys_prop_name)
+	VALUES("1", "Roughness"),
+    ("1","Thickness"),
+    ("2","Thickness"),
+    ("3","Surface Picture");
+    
+SELECT *
+FROM anlys_property;
+*/
+
+    
