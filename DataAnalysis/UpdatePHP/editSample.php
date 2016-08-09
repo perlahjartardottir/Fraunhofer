374a4946a61943c95f81ea582c2bcb5b5d6bf0ec
Perla Hjartardottir <phjartardottir@fraunhofer.org>
2016-08-09 17:20:36 -0400

    take out edit sample name

--- DataAnalysis/UpdatePHP/editSample.php
+++ DataAnalysis/UpdatePHP/editSample.php
@@ -2,12 +2,11 @@
 include '../../connection.php';
 
 $sampleID = mysqli_real_escape_string($link, $_POST["sampleID"]);
-$name = mysqli_real_escape_string($link, $_POST["name"]);
 $material = mysqli_real_escape_string($link, $_POST["material"]);
 $comment = mysqli_real_escape_string($link, $_POST["comment"]);
 
 $sql = "UPDATE sample
-		SET sample_name = '$name', sample_material = '$material', sample_comment='$comment'
+		SET sample_material = '$material', sample_comment='$comment'
 		WHERE sample_ID = '$sampleID';";
 $result = mysqli_query($link, $sql);
 
