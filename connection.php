c62d531b7c9a8e0b0be29138a20fb1399b0aca9f
Perla Hjartardottir <phjartardottir@fraunhofer.org>
2016-08-17 14:03:21 -0400

    connection.php

--- connection.php
+++ connection.php
@@ -11,4 +11,7 @@ if($link === false){
 // Change character set to utf8, for special characters like μ and °.
 mysqli_set_charset($link,"utf8");
 
+// Set file upload to max 100 MB. 
+ini_set('upload_max_filesize','100M');
+
 ?>
